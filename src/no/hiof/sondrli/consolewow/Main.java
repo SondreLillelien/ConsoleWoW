065d90d8bdc7c7caa12de75bc86ed74f799d9474
Sondre Lillelien <sondri@hiof.no>
2019-01-22 19:46:35 +0100

    Hei på deg

--- src/no/hiof/sondrli/consolewow/Main.java
+++ src/no/hiof/sondrli/consolewow/Main.java
@@ -1,8 +1,10 @@
 package no.hiof.sondrli.consolewow;
 
-public class Main {
+public class Main
+
+{
 
     public static void main(String[] args) {
-        System.out.println("hei");
+        System.out.println("hei på deg");
     }
 }
