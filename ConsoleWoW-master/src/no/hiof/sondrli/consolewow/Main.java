51f552a8d3e525cf8dc9881f088395b747264755
Mats Edvardsen <34913874+matsE1994@users.noreply.github.com>
2019-02-25 23:51:05 +0100

    fuck yall

---
+++ ConsoleWoW-master/src/no/hiof/sondrli/consolewow/Main.java
@@ -0,0 +1,20 @@
+package no.hiof.sondrli.consolewow;
+
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+        Random rng = new Random();
+        int level = rng.nextInt(5) + 1;
+        Warrior kreg = new Warrior("Kreg", 100, 20, 500, 15, level);
+        Warrior per = new Warrior ("Per", 100, 20, 500, 15, level);
+
+        System.out.println(per.getHp());
+
+        kreg.dealPhysDamage(per);
+
+        System.out.println(per.getHp());
+
+
+    }
+}
